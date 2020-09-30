3699ee3d1cfc72d2b43dececff43b54ca42bc0a4
Tech Youths <arsalanmohd237@gmail.com>
2020-09-30 23:36:45 +0530

    Updated Sieve

---
+++ Sieve of Eratosthenes.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+
+  cout << "Enter Number";
+  cin >> n;
+
+  bool prime[n+1];
+  memset(prime, true, sizeof(prime));
+    
+  for(int i = 2; i <= n; i++)
+  {
+    if(prime[i] == true)
+    {
+      for(int j = i*i; j <= n; j += i)
+        prime[j] = false;
+    }
+  }
+
+  for(int i=2;i<=n;i++)
+     if(prime[i]) cout<<i<<" ";
+  
+  return 0;
+}
