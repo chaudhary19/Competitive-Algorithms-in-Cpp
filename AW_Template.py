396d5720f608dca10c76d691dc7b5b61bec485ee
Suryanshu Singh <suryanshu15@gmail.com>
2020-09-30 21:49:43 +0530

    Revert "Added template for python"
    This reverts commit 979c8f379d660a880f9b399361c63a65759578d3.

--- AW_Template.py
+++
@@ -1,9 +0,0 @@
-import atexit, io, sys
-
-buffer = io.BytesIO()
-sys.stdout = buffer
-
-@atexit.register
-def write(): sys.__stdout__.write(buffer.getvalue())
-def get_ints(): return map(int, sys.stdin.readline().strip().split())
-def get_string(): return sys.stdin.readline().strip()
