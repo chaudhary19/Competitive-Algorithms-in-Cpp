aedb4d91f3de3c207911ade1b8f38aeb3fe0e129
Your Name <mynkgpt16@gmail.com>
2020-09-30 16:35:13 +0530

    change template for C++ Competitive Programmers

--- A_Template.cpp
+++ A_Template.cpp
@@ -1,12 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define int long long
-#define endl "\n"
+#define ar array
+#define ll long long
 
-int32_t main()
-{
-    IOS;
-    // write your code here
+const int MAX_N = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int rand(int l, int r) {
+    return uniform_int_distribution(l, r)(rng);
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
 }
