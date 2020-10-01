ff2d625acc57ed48ab8c7e8a626b8b04ee53650e
Tech Youths <arsalanmohd237@gmail.com>
2020-10-01 18:44:22 +0530

    Quick Sort

--- Quick Sort.cpp
+++ Quick Sort.cpp
@@ -40,8 +40,12 @@ int main()
     int n;
     cin >> n;
     int arr[n];
-    for(int i = 0; i<n; i++) cin >> arr[i];
+
+    for(int i = 0; i<n; i++) 
+        cin >> arr[i];
+
     quick(arr, 0, n-1);
     for(int i = 0; i<n; i++) cout << arr[i] << " ";
+
     return 0;
 }
