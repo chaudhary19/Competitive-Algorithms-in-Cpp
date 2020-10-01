b86f90815db32a2d12ca17c478eccdae653bfce8
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2020-10-01 11:32:38 +0530

    Update templateForc++.cpp

--- templateForc++.cpp
+++ templateForc++.cpp
@@ -1,48 +1,25 @@
 #include<bits/stdc++.h>
 #include<fstream>
-#define IO ios_base::sync_with_stdio(false);cin.tie(NULL);
-#define prime 1000000007
-#define pb push_back
-#define mp make_pair
-#define fo(i,a,b) for (int i = a; i <b; i++)
-#define forn(i,a,b) for (int i = a; i <=b; i++)
-#define num 1000000000
-#define ALL(v) v.begin(), v.end()
-#define INF INT_MAX
-typedef long long ll;
-#define TO ll t;cin>>t;while(TO--)
+#define IO                  ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define prime               1000000007
+#define pb                  push_back
+#define mp                  make_pair
+#define fo(i,a,b)           for (int i = a; i <b; i++)
+#define forn(i,a,b)         for (int i = a; i <=b; i++)
+#define ALL(v)              v.begin(), v.end()
+#define INF                 INT_MAX
+#define ll                  long long
+#define TO                  ll t;cin>>t;while(TO--)
+#define pii                 pair<int,int>
+#define vi                  vector<int>
+#define mii                 map<int,int>
+#define pqb                 priority_queue<int>
+#define pqs                 priority_queue<int,vi,greater<int> >
+#define ps(x,y)             fixed<<setprecision(y)<<x
+#define mk(arr,n,type)      type *arr=new type[n];
+
 using namespace std;
-
-//For getting SUM
-ll getSum(ll n) 
-{  
-   ll sum = 0; 
-   while (n != 0) 
-   { 
-       sum = sum + n % 10; 
-       n = n/10; 
-   } 
-   return sum; 
-}
-
-
-ll power(ll x, ll y, ll p)  
-{  
-    ll res = 1;     
-    x = x % p;   
-             
-    if (x == 0) return 0; 
-  
-    while (y > 0)  
-    {  
-        if (y & 1)  
-        res = (res*x) % p;
-        
-        y = y>>1; 
-        x = (x*x) % p;  
-    }  
-    return res;  
-} 
+using namespace __gnu_pbds;
 
 string getString(char x) 
 { 
@@ -51,6 +28,7 @@ string getString(char x)
 }
 
 int main()
+int32_t main()
  { 
    IO
    #ifndef ONLINE_JUDGE  
