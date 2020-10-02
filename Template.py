c9cbdd64f25d9c163f8c2e0b316c0f4fb9ba40e3
Tejaswika <teju050301@gmail.com>
2020-10-02 20:55:23 +0530

    Updated Template.py

--- Template.py
+++ Template.py
@@ -1,20 +1,11 @@
-# python program with fast I/O
-
-#module contain stdin ,stdout
-from sys import stdin, stdout 
-from time import perf_counter
-
-#integer input from user ,2 input in single line
-n,m=map(int,input().split()) 
-
-t1_start = perf_counter()
-
-for i in range(n):
-    t=int(stdin.readline()) # input using fast i/p method 
-    if t%m == 0:
-        stdout.write(str(t)+'\n') #input using fast o/p method
-
-t1_stop = perf_counter()# Stop the stopwatch
-
-print("Elapsed time:", t1_stop-t1_start) # Report results
-
+import sys
+input = sys.stdin.readline
+def inp():
+    return(int(input()))
+def inlt():
+    return(list(map(int,input().split())))
+def insr():
+    s = input()
+    return(list(s[:len(s) - 1]))
+def invr():
+    return(map(int,input().split()))
